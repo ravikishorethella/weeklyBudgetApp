1c5e27844704e787c175fe9c8a5c9548e84de53b
Ravi Kishore Thella <ravikishorethella@gmail.com>
2018-05-25 18:45:43 -0400

    validated the form fields

--- js/app.js
+++ js/app.js
@@ -1,8 +1,45 @@
 // classes
 
+class Budget {
+  constructor(budget) {
+    this.budget = Number(budget);
+    this.budgetLeft = this.budget;
+  }
+}
+
+// html content
+
+class HTML {
+  insertBudget(amount) {
+    budgetTotal.innerHTML = `${amount}`;
+    budgetLeft.innerHTML = `${amount}`;
+  }
+  //   error message
+  printMessage(message, className) {
+    const messageWrapper = document.createElement("div");
+    messageWrapper.classList.add("text-center", "alert", className);
+    messageWrapper.appendChild(document.createTextNode(message));
+
+    // inserting the message above the form
+    document
+      .querySelector(".primary")
+      .insertBefore(messageWrapper, addExpenseForm);
+
+    //   clearing the error message
+    setTimeout(() => {
+      document.querySelector(".primary .alert").remove();
+      addExpenseForm.reset();
+    }, 3000);
+  }
+}
+
 // variables
-const addExpenseForm = document.getElementById("add-expense");
+const addExpenseForm = document.getElementById("add-expense"),
+  budgetTotal = document.querySelector("span#total"),
+  budgetLeft = document.querySelector("span#left");
 let budget, userBudget;
+
+const html = new HTML();
 
 // event listeners
 eventListeners();
@@ -17,11 +54,23 @@ function eventListeners() {
     if (userBudget === null || userBudget === "" || userBudget === "0") {
       window.location.reload();
     } else {
+      budget = new Budget(userBudget);
+
+      //   html class instantiation
+      html.insertBudget(budget.budget);
     }
   });
 
   // when user adds an expense
   addExpenseForm.addEventListener("submit", function(e) {
     e.preventDefault();
+    //   reading the values from the form
+    const expenseName = document.getElementById("expense").value;
+    const amount = document.getElementById("amount").value;
+    if (expenseName === "" || amount === "") {
+      html.printMessage("All the fields are mandatory", "alert-danger");
+    } else {
+      console.log("valid");
+    }
   });
 }
